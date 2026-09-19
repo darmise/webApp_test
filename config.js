671b0e5855c6c42666dcf0f536e1bc9c7741c58b
darmise <74202928+darmise@users.noreply.github.com>
2026-09-19 12:03:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@
 
 const CONFIG = {
   // Google Cloud Console > API e servizi > Credenziali > ID client OAuth
-  GOOGLE_CLIENT_ID: "559443131366-7lo2fa3jv9u975c1mfhe0cocutacnih8.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "1054897318345-fqkg9t81ag25tn566hk5662fk8oo06b0.apps.googleusercontent.com",
 
   // Dall'URL del foglio: https://docs.google.com/spreadsheets/d/QUESTO/edit
-  SPREADSHEET_ID: "INSERISCI_LO_SPREADSHEET_ID",
+  SPREADSHEET_ID: "1Hr8wGG10SpNrxSa1iX-JnnRGUnaU_8Uik1xZjdNArNY",
 
   // Nome del tab e intervallo da leggere (riga 1 = intestazioni colonne)
   SHEET_RANGE: "Foglio1!A1:Z1000",
